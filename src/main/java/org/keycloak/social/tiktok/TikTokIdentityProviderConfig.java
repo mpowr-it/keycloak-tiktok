9226e1b8ae10017d68eed129d65430e5c4abbefc
Holger Woltersdorf <hw@hollo.me>
2025-04-24 08:34:29 +0200

    Remove unused imports

--- src/main/java/org/keycloak/social/tiktok/TikTokIdentityProviderConfig.java
+++ src/main/java/org/keycloak/social/tiktok/TikTokIdentityProviderConfig.java
@@ -3,20 +3,12 @@ package org.keycloak.social.tiktok;
 import org.keycloak.broker.oidc.OAuth2IdentityProviderConfig;
 import org.keycloak.models.IdentityProviderModel;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 public class TikTokIdentityProviderConfig extends OAuth2IdentityProviderConfig {
 
     public TikTokIdentityProviderConfig(IdentityProviderModel model) {
         super(model);
     }
 
-    public TikTokIdentityProviderConfig() {}
-
-    public void setPrompt(String prompt) {
-        getConfig().put("prompt", prompt);
+    public TikTokIdentityProviderConfig() {
     }
 }
